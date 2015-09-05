9a1b06ce47901dd4e17d3e1eca1320588c6f5f0e
David Bui <dhbdhb2000@gmail.com>
2015-09-05 12:12:05 -0400

    Soundcloud search extracted to new js file

---
+++ soundcloud/track_search.js
@@ -0,0 +1,9 @@
+SC.initialize({
+  client_id: 'cb32f7702a0bd6053ef890bdf200525a'
+});
+// find all sounds of buskers
+SC.get('/tracks', { q: 'kygo'}, function(tracks) {
+  console.log(tracks);
+});
+
+alert("HELLO!");
