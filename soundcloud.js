232855a07ff09def2ec521f245c995f1f48b1da7
Stefanie Alfonso <astef@seas.upenn.edu>
2015-09-05 09:04:30 -0400

    hello world node server

--- soundcloud.js
+++ soundcloud.js
@@ -1,6 +1,10 @@
+var myFirebaseRef = new Firebase("https://charityjuke.firebaseio.com/");
+var clientSecret = 'd8f69c7e7c5e5b7119876aba9382a5d5';
+var clientId = 'cb32f7702a0bd6053ef890bdf200525a';
+
 // initialize client with app credentials
 SC.initialize({
-  client_id: 'YOUR_CLIENT_ID',
+  client_id: clientId,
   redirect_uri: 'http://example.com/callback'
 });
 
