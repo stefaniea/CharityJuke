4ef3ded54e6bd211f0cff4146e7b6838e050652c
David Bui <dhbdhb2000@gmail.com>
2015-09-05 16:25:00 -0400

    removed alert

--- soundcloud/track_search.js
+++ soundcloud/track_search.js
@@ -1,6 +1,5 @@
 SC.initialize({
   client_id: 'cb32f7702a0bd6053ef890bdf200525a',
-  redirect_uri: 'http://example.com/callback'
 });
 
 
@@ -12,7 +11,6 @@ function newSearch() {
 *   Search for tracks with the given searchTerm
 */
 function search(searchTerm) {
-	alert(searchTerm);
 	SC.get('/tracks', { q: searchTerm}, function(tracks) {
 		display(tracks);
 		console.log(tracks);
