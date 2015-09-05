70435bce1c3213cef9615f1eba1704e839721cdb
David Bui <dhbdhb2000@gmail.com>
2015-09-05 12:57:15 -0400

    fixed merge conflicts

--- soundcloud/track_search.js
+++ soundcloud/track_search.js
@@ -1,7 +1,30 @@
 SC.initialize({
   client_id: 'cb32f7702a0bd6053ef890bdf200525a'
 });
-// find all sounds of buskers
-SC.get('/tracks', { q: 'kygo'}, function(tracks) {
-  console.log(tracks);
-});
+
+search("kygo");
+
+/** 
+*   Search for tracks with the given searchTerm
+*/
+function search(searchTerm) {
+	SC.get('/tracks', { q: searchTerm}, function(tracks) {
+	  display(tracks);
+	  console.log(tracks);
+	});	
+}
+
+function display(jsonTracks) {
+	console.log("Tracks:");
+	jsonTracks.forEach(function(track) {
+		console.log(track.stream_url); 
+
+
+		var track_url = 'http://soundcloud.com/forss/flickermood';
+		SC.oEmbed(track_url, { auto_play: true }, function(oEmbed) {
+		console.log('oEmbed response: ' + oEmbed);
+		});
+
+	});
+}
+
