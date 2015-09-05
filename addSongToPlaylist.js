df4a63797b896276d0a5ce4990a12c16138d27b9
Stefanie Alfonso <astef@seas.upenn.edu>
2015-09-05 11:47:40 -0400

    firebase in html

--- addSongToPlaylist.js
+++ addSongToPlaylist.js
@@ -1,5 +1,9 @@
 var myFirebaseRef = new Firebase("https://charityjuke.firebaseio.com/");
 
 function addSongToPlaylist(playlist_id) {
-	
-}
+	myFirebaseRef.on("value", function(snapshot) {
+	  console.log(snapshot.val());
+	}, function (errorObject) {
+	  console.log("The read failed: " + errorObject.code);
+	});
+};
